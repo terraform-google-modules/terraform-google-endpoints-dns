88b48d3020e9e018a95d397eb6a2ea8fb419252e
David Bingham <djbingham@users.noreply.github.com>
2022-06-14 14:32:08 +0100

    Remove jsonencode from create_cmd_body
    The script doesn't seem to receive the arguments correctly. Maybe
    Terraform handles the json encoding itself.

--- main.tf
+++ main.tf
@@ -56,10 +56,10 @@ module "module-cloudep-dns-prep" {
   skip_download = var.skip_gcloud_download
 
   create_cmd_entrypoint = "${path.module}/scripts/cloudep_prep.sh"
-  create_cmd_body       = jsonencode({
+  create_cmd_body       = {
     endpoint = local.service_name
     project  = local.project
-  })
+  }
 
   destroy_cmd_entrypoint = ":"
   destroy_cmd_body       = ""
